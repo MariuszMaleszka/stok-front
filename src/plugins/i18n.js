9ba6fab1878e05806b2c6ca120d1f13464c910b6
Mariusz <mariusz.maleszka@saloner.pl>
2025-12-16 09:32:43 +0100

    Add date-fns adapter for Vuetify; enhance i18n integration and localization support

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -1,18 +1,29 @@
 import { createI18n } from 'vue-i18n'
 import { useCookies } from '@vueuse/integrations/useCookies'
 
-import pl from './i18n/locales/pl.json'
-import en from './i18n/locales/en.json'
+import plJson from './i18n/locales/pl.json'
+import enJson from './i18n/locales/en.json'
+import { pl as plVuetify, en as enVuetify } from 'vuetify/locale'
 
 const cookies = useCookies(['locale'])
 const savedLocale = cookies.get('locale') || 'pl'
 
+const messages = {
+  en: {
+    ...enJson,
+    $vuetify: enVuetify
+  },
+  pl: {
+    ...plJson,
+    $vuetify: plVuetify
+  }
+}
+
 export default createI18n({
-  legacy: false, // Use Composition API mode
+  legacy: false,
   locale: savedLocale,
   fallbackLocale: 'en',
-  messages: {
-    pl,
-    en
-  }
+  messages,
+  silentTranslationWarn: true,
+  missingWarn: false
 })
